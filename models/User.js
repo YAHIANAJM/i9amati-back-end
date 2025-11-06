826f1607e28ec10cbfa0b0fe54b6d105a4f1d045
YAHIANAJM <yahyafreeninja@gmail.com>
2025-11-06 21:02:34 +0100

    user model

--- models/User.js
+++ models/User.js
@@ -3,6 +3,8 @@ import mongoose from 'mongoose';
 const UserSchema = new mongoose.Schema({
   firstName: { type: String, required: true },
   lastName: { type: String, required: true },
+  email: { type: String, required: true, unique: true }, // ✅ ADD THIS
+  password_hash: { type: String, required: true }, 
   nationalId: { type: String }, // e.g., BW123****
   role: { type: String, enum: ['supervisor', 'union_agent', 'property_owner'], required: true },
   apartment: { type: mongoose.Schema.Types.ObjectId, ref: 'Apartment' },
