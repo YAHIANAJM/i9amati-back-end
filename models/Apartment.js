b053a09ea3d4f528b526321aaebd8844d78fbd65
YAHIANAJM <yahyafreeninja@gmail.com>
2025-11-09 16:08:30 +0100

    fix

--- models/Apartment.js
+++ models/Apartment.js
@@ -2,17 +2,14 @@
 import mongoose from 'mongoose';
 
 const ApartmentSchema = new mongoose.Schema({
-<<<<<<< HEAD
   number: { type: String, required: true },
   floor: { type: Number, required: true },
   space: { type: Number, required: true },
-=======
   apartment_number: { type: String, required: true }, // clearer name
   floor: { type: Number },        // optional
   space: { type: Number },        // optional (in m²)
   name: { type: String },
   type: { type: String, default: 'residential' },
->>>>>>> yahyabackend
   agent: { type: mongoose.Schema.Types.ObjectId, ref: 'UnionAgent', required: true },
   building: { type: mongoose.Schema.Types.ObjectId, ref: 'Building', required: true },
   owners: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
